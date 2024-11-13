e4754f0c49fe27155548228af5dd1e848da6957c
Jack D. Law <114697986+DioJoesta@users.noreply.github.com>
2024-11-13 12:59:51 +0000

    initial to do list

---
+++ Listly/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState,useEffect } from 'react'
+import ToDoList from './Todolist';
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <ToDoList/>
+    </>
+  )
+}
+
+export default App
